bf97fcfd85ceaef51f3c9ee6a6d1f6eccbc4f3ec
Timor666 <35324214+Timor666@users.noreply.github.com>
2018-09-29 17:34:14 +0800

    增加递归

---
+++ 项目源码/C#源码/递归函数/递归函数/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 递归函数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //计算5天
+
+
+           // Console.WriteLine("最终值" + FunFor(5));
+
+          Console.WriteLine("最终值" + FunRecursion(5));
+
+
+
+
+            Console.ReadKey();
+        }
+
+        //For语句
+        static int FunFor(int date)
+        {
+            int sum = 0;
+            for (int i = 1; i <= date; i++)
+            {
+                sum += i;
+                Console.WriteLine(i);
+            }
+            return sum;
+        }
+
+        //递归函数
+        static int FunRecursion(int date)
+        {
+            Console.WriteLine(date);
+            if(date == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return FunRecursion(date-1) + date;
+            }
+        }
+    }
+}
